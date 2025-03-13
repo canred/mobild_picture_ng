cbc6fb2bfc29c2d9aaf91620125db47e9883ebd4
陳慧鴻 <canred.chen@gmail.com>
2025-03-14 02:12:02 +0800

    環境設定

--- main/src/app/app-config.ts
+++ main/src/app/app-config.ts
@@ -1,7 +1,30 @@
 import { A } from "@angular/cdk/keycodes";
+import { environment } from '../environments/environment';
+
+const ENV = environment.NODE_ENV as 'development' | 'production';
+console.log('ENV:', ENV);
+
+interface Config {
+  BASE_URL: string;
+  SYSTEM_VERSION: string;
+}
+
+const CONFIG: { [key in 'development' | 'production']: Config } = {
+  development: {
+    BASE_URL: 'http://localhost:3001',
+    SYSTEM_VERSION: '1.0.0',
+  },
+  production: {
+    BASE_URL: 'http://localhost:3002',
+    SYSTEM_VERSION: '1.0.0',
+  }
+};
+
+const CURRENT_CONFIG = CONFIG[ENV];
 
 export const APP_CONFIG = {
-  SYSTEM_VERSION: '1.0.0',
-  API_ROOT: 'http://localhost:3000/api',
-  API_WEB_ROOT: 'http://localhost:3000',
+  SYSTEM_VERSION: CURRENT_CONFIG.SYSTEM_VERSION,
+  API_ROOT: `${CURRENT_CONFIG.BASE_URL}/api`,
+  API_WEB_ROOT: CURRENT_CONFIG.BASE_URL,
+  BROWSER_TARGET: environment.browserTarget
 };
